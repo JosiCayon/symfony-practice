f683212314db42c08b918ccf76173d870a6169a6
luisglezbrown <lfernanglez@gmail.com>
2021-06-11 13:06:25 +0200

    Añadir plantilla para la index del controllerDefau

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+// El namespace será App\NombreCarpetaQueEstamos
+namespace App\Controller;
+
+// Esto lo importa automáticamente al declarar Response en la clase
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+// Hay que importar este Route
+use Symfony\Component\Routing\Annotation\Route;
+
+// La clase se debe llamar igual que el fichero
+// AbstractController pone a nuestra disposición varias funciones
+class DefaultController extends AbstractController {
+
+    /**
+     * @Route("/default", name="default_index")
+     * 
+     * El primer parámetro de Route es la URL a la que
+     * queremos asociar la acción y lo segundo es el nombre
+     * que queremos dar a la ruta.
+    */
+
+    // Debe tener un método público que siempre debe devolver algo
+    public function index(): Response 
+    {
+        // Por defecto debe ser un objeto de la clase: Response (Symfony\Component\HttpFoundation)
+        // render() es un método hereado de AbstractController
+        // que devuelve el contenido declarado en una plantillas de Twig.
+        // https://twig.symfony.com/doc/3.x/templates.html
+
+        $name = 'Luis';
+
+        //Lo primero es la vista y lo segundo los parámetros.
+        //Los parámetros van como un array asociativo
+        return $this->render('default/index.html.twig', [
+            'nombre'=>$name
+        ]);
+    }
+
+}
